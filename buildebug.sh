Backup-Gits/android_kernel_xiaomi_sm8350

build_script: Pack anykernel zip automatically

buildebug.sh
@@ -43,3 +43,40 @@ DIFF=$(($DATE_END - $DATE_START))
 echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 echo
 ls -a $ZIMAGE_DIR
+
+cd $KERNEL_DIR
+if grep -q "error: " kernel.log
+then
+	echo; echo; grep -n "error: " kernel.log; echo "\n\n"
+	chown -R nakixii *
+	chgrp -R nakixii *
+	exit 0
+elif grep -q "undefined reference to" kernel.log
+then
+	echo; echo; grep -n "undefined reference to" kernel.log; echo "\n\n"
+	chown -R nakixii *
+	chgrp -R nakixii *
+	exit 0
+elif grep -q "undefined symbol" kernel.log
+then
+	echo; echo; grep -n "undefined symbol" kernel.log; echo "\n\n"
+	chown -R nakixii *
+	chgrp -R nakixii *
+	exit 0
+elif grep -q "Error 2" kernel.log
+then
+	exit 0
+else
+TIME="$(date "+%Y%m%d-%H%M%S")"
+mkdir -p tmp
+cp -fp $ZIMAGE_DIR/Image tmp
+cp -rp ./anykernel/* tmp
+cd tmp
+7za a -mx9 $TIME-tmp.zip *
+cd ..
+rm DynamIQ*.zip
+cp -fp tmp/$TIME-tmp.zip DynamIQ-Kernel-Mi11-$TIME.zip
+rm -rf tmp
+chown -R nakixii *
+chgrp -R nakixii *
+fi

VladV1V/kernel_xiaomi_renoir

buildebug.sh
@@ -42,3 +42,40 @@ DIFF=$(($DATE_END - $DATE_START))
 echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 echo
 ls -a $ZIMAGE_DIR
+
+cd $KERNEL_DIR
+if grep -q "error: " kernel.log
+then
+	echo; echo; grep -n "error: " kernel.log; echo "\n\n"
+	chown -R vlad *
+	chgrp -R vlad *
+	exit 0
+elif grep -q "undefined reference to" kernel.log
+then
+	echo; echo; grep -n "undefined reference to" kernel.log; echo "\n\n"
+	chown -R vlad *
+	chgrp -R vlad *
+	exit 0
+elif grep -q "undefined symbol" kernel.log
+then
+	echo; echo; grep -n "undefined symbol" kernel.log; echo "\n\n"
+	chown -R vlad *
+	chgrp -R vlad *
+	exit 0
+elif grep -q "Error 2" kernel.log
+then
+	exit 0
+else
+TIME="$(date "+%Y%m%d-%H%M%S")"
+mkdir -p tmp
+cp -fp $ZIMAGE_DIR/Image tmp
+cp -rp ./anykernel/* tmp
+cd tmp
+7za a -mx9 $TIME-tmp.zip *
+cd ..
+rm DynamIQ*.zip
+cp -fp tmp/$TIME-tmp.zip Noob-Kernel-Mi11Lite5G-$TIME.zip
+rm -rf tmp
+chown -R vlad *
+chgrp -R vlad *
+fi